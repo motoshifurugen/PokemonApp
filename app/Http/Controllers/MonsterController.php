b5ae52949638b5f637ea2e368a13863c6e99d5bc
motoshifurugen <amanogawa_m@icloud.com>
2020-05-29 16:00:30 +0900

    add vue.js

--- app/Http/Controllers/MonsterController.php
+++ app/Http/Controllers/MonsterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Monster;
+use App\Attribute;
+use App\Region;
 use Illuminate\Http\Request;
 
 class MonsterController extends Controller{
@@ -17,11 +19,23 @@ class MonsterController extends Controller{
     public function store(Request $request) {
         $monster = new Monster();
         $monster->name = request('name');
+        $monster->attribute_id = request('attribute_id');
+        $monster->region_id = request('region_id');
+        $monster->size = request('size');
+        $monster->weight = request('weight');
+        $monster->attack_name = request('attack_name');
+        $monster->attack_description = request('attack_description');
         return $monster->save();
     }
 
     public function update(Request $request, Monster $monster) {
         $monster->name = request('name');
+        $monster->attribute_id = request('attribute_id');
+        $monster->region_id = request('region_id');
+        $monster->size = request('size');
+        $monster->weight = request('weight');
+        $monster->attack_name = request('attack_name');
+        $monster->attack_description = request('attack_description');
         $monster->save();
     }
 
